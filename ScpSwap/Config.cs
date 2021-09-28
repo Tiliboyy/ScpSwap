dad3ac7bf8a8233a33f084f7b91b34d48c752e9e
Build <jwbradley2003@gmail.com>
2021-09-28 13:57:13 -0400

    Initial Upload

---
+++ ScpSwap/Config.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Config.cs" company="Build">
+// Copyright (c) Build. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ScpSwap
+{
+    using Exiled.API.Interfaces;
+
+    /// <inheritdoc />
+    public class Config : IConfig
+    {
+        /// <inheritdoc />
+        public bool IsEnabled { get; set; } = true;
+    }
+}
---
+++ ScpSwap/Plugin.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Plugin.cs" company="Build">
+// Copyright (c) Build. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ScpSwap
+{
+    using Exiled.API.Features;
+
+    /// <summary>
+    /// The main plugin class.
+    /// </summary>
+    public class Plugin : Plugin<Config>
+    {
+    }
+}
