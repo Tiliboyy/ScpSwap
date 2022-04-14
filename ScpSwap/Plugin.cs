e58fae6d94727eced732b85c7594e7ee44909664
Build <jwbradley2003@gmail.com>
2022-04-13 23:20:22 -0400

    Bump version, bump Exiled version

--- ScpSwap/Plugin.cs
+++ ScpSwap/Plugin.cs
@@ -35,10 +35,10 @@ namespace ScpSwap
         public override string Prefix => "ScpSwap";
 
         /// <inheritdoc />
-        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 1, 3);
 
         /// <inheritdoc />
-        public override Version Version { get; } = new Version(1, 1, 1);
+        public override Version Version { get; } = new Version(1, 1, 2);
 
         /// <inheritdoc />
         public override void OnEnabled()
